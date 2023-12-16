5254716a54b84cd823fd0d4bbc190af4b70daab7
tsegaye27 <ttalegn29@gmail.com>
2023-12-16 20:57:32 +0300

    included a handler for deleting items

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
 import Logo from "./components/Logo";
 import PackingList from "./components/PackingList";
@@ -6,15 +6,23 @@ import Stats from "./components/Stats";
 
 export default function App() {
   const [items, setItems] = useState([]);
+  useEffect(() => {
+    document.title = "Far Away";
+  }, []);
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
