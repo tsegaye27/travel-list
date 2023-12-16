08505142bd2079f192316c15aa9d5db59c70793a
tsegaye27 <ttalegn29@gmail.com>
2023-12-16 22:05:12 +0300

    calculated the ratio of numItems to numPackedItems

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,11 +1,16 @@
 import Item from "./Item";
 
-export default function PackingList({ items, onDeleteItem }) {
+export default function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
         ))}
       </ul>
     </div>
