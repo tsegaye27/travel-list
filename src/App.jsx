bc959ade69cee04efe459af0ef0ac83b7b99390f
tsegaye27 <ttalegn29@gmail.com>
2023-12-16 20:21:24 +0300

    lifted up the state to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import Form from "./components/Form";
 import Logo from "./components/Logo";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
