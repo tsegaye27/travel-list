4e132e5b3893c146433f18f69ef013ff66722fe3
tsegaye27 <ttalegn29@gmail.com>
2023-12-16 09:46:17 +0300

    added a style to the div

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import Form from "./components/Form";
+import Logo from "./components/Logo";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
