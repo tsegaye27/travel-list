66c330354710c4e3746c572967f0c3cf3cca9ce5
tsegaye27 <ttalegn29@gmail.com>
2023-12-16 09:48:47 +0300

    included some styling to the stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,1 +1,7 @@
-export default function Stats() {}
+export default function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜You have X items on your list, and you have packed X (X%)</em>
+    </footer>
+  );
+}
