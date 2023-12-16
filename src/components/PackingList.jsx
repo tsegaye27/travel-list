a9c735d246f8eb01600e32440c1470b69bfcd237
tsegaye27 <ttalegn29@gmail.com>
2023-12-16 20:58:49 +0300

    received and passed the onDeleteItem handler

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,17 +1,11 @@
 import Item from "./Item";
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: false },
-//   { id: 3, description: "Charger", quantity: 1, packed: false },
-// ];
-
-export default function PackingList({ items }) {
+export default function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
         ))}
       </ul>
     </div>
