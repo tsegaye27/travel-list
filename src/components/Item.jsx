fad90182d9fff889f1eac6a81c95da21e3fbb05b
tsegaye27 <ttalegn29@gmail.com>
2023-12-16 22:02:58 +0300

    saved the numPackedItem value

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,6 +1,13 @@
-export default function Item({ item, onDeleteItem }) {
+export default function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onClick={() => {
+          onToggleItem(item.id);
+        }}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
