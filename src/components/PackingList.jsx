08370864ddb5f623d79d1198842530802201f350
tsegaye27 <ttalegn29@gmail.com>
2023-12-16 20:22:36 +0300

    received a prop to create an Item component

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,16 +1,16 @@
 import Item from "./Item";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Charger", quantity: 1, packed: false },
+// ];
 
-export default function PackingList() {
+export default function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
